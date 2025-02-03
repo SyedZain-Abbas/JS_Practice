aff8992067ede8f03e0a42330131c38357765c0c
zain abbas <zaidizainabbas3@gmail.com>
2025-02-03 19:38:15 +0530

    string CharCode

---
+++ stringSort.js
@@ -0,0 +1,16 @@
+const arr = "zaman"
+
+
+let y = arr.split("")
+console.log(y);
+//let y = arr.split(" , ")
+console.log(arr.charCodeAt(1));
+//console.log(y.length);
+console.log(typeof arr);
+let x = arr.split("").forEach((element) => {
+        console.log(element.charCodeAt(element))
+});
+console.log(x);
+
+
+
